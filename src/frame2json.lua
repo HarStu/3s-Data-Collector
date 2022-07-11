800db374b9208797bd8b6b9a12f3a8c4edf3fad3
h <harrisonmstuart@gmail.com>
2022-07-11 19:04:59 -0400

    update frame2json.lua

--- src/frame2json.lua
+++ src/frame2json.lua
@@ -1,28 +1,30 @@
 --[[
-	Lua script tasked with capturing a game-state json from a single frame
+	Lua script tasked with capturing a game-state json from a single frame. 
+	Strictly a test right now.
+
+	player 1: located at 0x02068C6C
+	player 2: located at 0x02069104
 ]]
 
-require("libs/dkjson")
+json = require ("./lua-libs/dkjson")
 
-frame = {}
-frame.p1 = {}
-frame.p2 = {}
+p1_location = 0x02068C6C
 
+game_data = {}
 
+-- collect a snapshot of game data and return it as a table
+function take_snapshot()
+	return {
+		frame_number = memory.readdword(0x2007F00),
+		p1_posture = memory.readbyte(p1_location + 0x20E)
+	}
+end
+
+-- every frame, take a snapshot, convert it to a json, and output such. 
 while emu.frameadvance() do
-	frame.p1.health = 
-	--frame.p2.health = 
-	--frame.p1.meter =
-	--frame.p2.meter = 
-	--frame.p1.gauge = 
-	--frame.p2.gauge = 
-	--frame.p1.stun = 
-	--frame.p2.stun = 
-	--frame.clock = 
-
-
-
-
-
+	table_frame = take_snapshot
+	json_frame = json.encode(frame, {indent = true})
+	print(json_frame)
+end
 
 print(frame)
