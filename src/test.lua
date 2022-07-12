2a50d0cee08e56f97b8c59eaf673208acb7e3e32
h <harrisonmstuart@gmail.com>
2022-07-12 15:22:43 -0400

    Update test.lua
    test.lua now contains a rudimentary system which can capture information
    about a frame and output it to an external file

--- src/test.lua
+++ src/test.lua
@@ -1,42 +1,56 @@
 --[[
 	testing lua scripting capabilities 
+
+	TODOS:
+		file out
+			write to file rather than command line
+			write to file in JSON format
+		additional data
+			ingame timer
+			p1 health
+			p2 health
+			(more to come)
+		verification
+			it can't be assumed that registerafter is only called once per frame
+			so make sure that the frame_number value is unique before adding the data to the output
+
+
 ]]
 
 -- file must be present in /fbneo/lua
 local json = require ("dkjson")
-
 
 -- important memory addresses
 p1_base = 0x02068C6C
 p2_base = 0x02069104
 frame_number = 0x2007F00 --dword
 
+
 -- verify that the script has started
 emu.print("hello world")
 
-function get_frame_number()
-	return memory.readdword(frame_number)
+-- setup the output file
+output_file = io.open("../data/log.txt", "w")
+
+
+function on_start() 
+	output_file:write("script started!\n\n")
 end
 
--- return a single value from memory. eventually this will return a table of values
-function snapshot()
-	-- confirm the frame_number value first. 
-	start_snapshot_frame_number = memory.readdword(frame_number)
+function on_exit()
+	print("all done!")
+	output_file:close()
 end
 
--- function to test emulator pausing capability
--- currently, calling this (even with the pause/unpause commented out!) crashes the emulator
-function pause_and_unpause()
-	emu.print("entered into the pause and unpause function")
-	current_frame = get_frame_number()
-	unpause_frame = current_frame + 60
-	emu.print("starting on frame " .. tostring(current_frame) .. "and ending on frame " .. tostring(unpause_frame))
-	-- emu.pause()
-	while current_frame < unpause_frame do
-		current_frame = get_frame_number()
-		emu.print("the current frame is")
-		emu.print(tostring(current_frame))
-		emu.print("\n")
-	end 
-	-- emu.unpause()
+function per_frame()
+	emu_frame = emu.framecount()
+	ingame_frame = memory.readdword(frame_number)
+
+	output_file:write("INGAME FRAME: " .. tostring(ingame_frame) .. "\n")
+	output_file:write("\n")
+
 end
+
+emu.registerstart(on_start)
+emu.registerexit(on_exit)
+emu.registerafter(per_frame)
