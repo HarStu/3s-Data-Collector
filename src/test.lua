a638903fec4005dfb225d7de74ec5d5216986103
h <harrisonmstuart@gmail.com>
2022-07-12 18:17:29 -0400

    Add json output functionality to test.lua
    test.lua will now output a log.json along with a log.txt; however, this
    json will only be filled out if the lua script is manually halted, and
    there's currently no function for cleanly ending the lua script at the
    end of the replay

--- src/test.lua
+++ src/test.lua
@@ -1,6 +1,10 @@
 --[[
 	testing lua scripting capabilities 
 
+	NOTES:
+		emu.framecount() persists even through resets
+		the ingame frame_number does not 
+
 	TODOS:
 		file out
 			write to file in JSON format
@@ -10,8 +14,8 @@
 			it can't be assumed that registerafter is only called once per frame, so make sure that the frame_number value is unique before adding the data to the output
 ]]
 
--- file must be present in /fbneo/lua
-local json = require ("dkjson")
+-- if there's issues, try adding dkjson.lua to /fbneo/lua
+local json = require ("libs/dkjson")
 
 -- important memory addresses
 p1_base = 0x02068C6C
@@ -23,22 +27,35 @@ match_state = 0x020154A7 --byte
 -- add these to the base addresses to get the relevant value for each player
 life_offset = 0x9F --byte
 
--- setup the output file
-output_file = io.open("../data/log.txt", "w")
+-- setup the output files
+output_txt = io.open("../data/log.txt", "w")
+output_json = io.open("../data/log.json", "w")
 
+-- output table
+output_table = {}
 
+-- runs when emulation starts, or is reset
 function on_start() 
-	output_file:write("script started!\n\n")
+	output_txt:write("\nemulation started!\n\n")
 end
 
+-- runs when the lua is stopped
+-- note that this isn't properly occur if the window is closed early!
 function on_exit()
-	output_file:close()
+	output_table_json_string = json.encode(output_table, {indent = true})
+	output_json:write(output_table_json_string)
+
+	output_txt:write("just a quick test that on_exit is being entered")
+	output_txt:close()
+	output_json:close()
 end
 
+-- runs every frame (currently at the end)
 function per_frame()
 
 	-- capture information about the game
 	ingame_frame = memory.readdword(frame_number)
+	emu_frame = emu.framecount()
 
 	p1_life = memory.readbyte(p1_base + life_offset)
 	p2_life = memory.readbyte(p2_base + life_offset)
@@ -50,12 +67,24 @@ function per_frame()
 		in_match = "no"
 	end
 
-	-- write captured information to output file
-	output_file:write("frame: " .. tostring(ingame_frame) .. "\n")
-	output_file:write("p1 life: " .. tostring(p1_life) .. "\n")
-	output_file:write("p2 life: " .. tostring(p2_life) .. "\n")
-	output_file:write("in match?: " .. tostring(in_match) .. "\n")
-	output_file:write("\n")
+	-- write captured information to output_txt
+	output_txt:write("ingame frame: " .. tostring(ingame_frame) .. "\n")
+	output_txt:write("emu frame: " .. tostring(emu_frame) .. "\n")
+	output_txt:write("p1 life: " .. tostring(p1_life) .. "\n")
+	output_txt:write("p2 life: " .. tostring(p2_life) .. "\n")
+	output_txt:write("in match?: " .. tostring(in_match) .. "\n")
+	output_txt:write("\n")
+
+	-- write captured information to a frame table
+	frame = {}
+	frame["p1 life"] = p1_life
+	frame["p2 life"] = p2_life
+	frame["ingame frame"] = ingame_frame
+	frame["in match"] = in_match
+	
+	-- add the frame table to the output table
+	-- since the emu framecount is persistent across resets, it will serve as the key 
+	output_table[emu_frame] = frame
 end
 
 emu.registerstart(on_start)
