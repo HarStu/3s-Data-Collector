bd00757a33afadf1aac2d5c36004d7d394c1f3a3
h <harrisonmstuart@gmail.com>
2022-07-14 17:59:16 -0400

    Update 3sDC.py to load from .config.json and output to .working.json

--- src/3sDC.py
+++ src/3sDC.py
@@ -1,36 +1,48 @@
-import sys
-import json
-
 """
 Starts a replay w/ the scraping lua running. To scrape a single game, run as follows:
-    python3 start.py <replays json> <challenge id>
+    python3 3sDC.py <challenge id>
 
 
 TODO:
     immediately
         after .working.json has been created, start the emulator with the scraping lua
-    down 
+        retrieve the json database being worked on, and the location of the fc install, from ../data/.config.json
+    down the line
         add functionality to fetch and scrape replays back-to-back, rather than having to do each one individually
 """
+import sys
+import json
+import os
 
-json_database = None
+
 challenge_id = None
+
 
 # handle command line arguments
 argv_len = len(sys.argv)
-if argv_len != 3:
+if argv_len != 2:
     print("invalid number of arguments!")
-    print("you've provided {length} args".format(length = len(sys.argv)))
     sys.exit()
 else:
-    json_database = sys.argv[1]
-    challenge_id = sys.argv[2]
+    challenge_id = sys.argv[1]
 
-def scrape_individual_replay(_json_database, _challenge_id):
+# run a single replay w/ the scraper lua
+def scrape_individual_replay(_challenge_id):
 
-    print("Scraping {game} from {database}".format(game = _challenge_id, database = _json_database))
+    # fetch information from ../data/.config.json
+    print("loading from config")
+
+    with open("../data/.config.json", "r") as c:
+        config = json.load(c)
+
+    fcadefbneo = config["fcadefbneo_path"]
+    json_database = "../data/" + config["replay_json_database_filename"]
+
+    # grab game which is going to be scraped, and output it to .working.json
+    # when scraper.lua runs, it will read from .working.json
+    print("Scraping {game} from {database}".format(game = _challenge_id, database = json_database))
     
-    with open(_json_database, "r") as j:
+    with open(json_database, "r") as j:
         replay_database = json.load(j)
     target_game = replay_database[_challenge_id]
 
@@ -41,5 +53,4 @@ def scrape_individual_replay(_json_database, _challenge_id):
     with open("../data/.working.json", "w") as w:
         json.dump(target_game, w, ensure_ascii = False, indent = 4)
 
-
-scrape_individual_replay(json_database, challenge_id)
+scrape_individual_replay(challenge_id)
