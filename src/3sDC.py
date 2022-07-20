10dd240c3cb8b50c2c1063a728637aca2f89a9aa
h <harrisonmstuart@gmail.com>
2022-07-19 20:46:45 -0400

    Update 3sDC.py header comment TODO and main()

--- src/3sDC.py
+++ src/3sDC.py
@@ -2,7 +2,8 @@
 Entry point for 3s Data Collector
 
 TODO:
-    tasks in replayscraper.py and replayfetcher.py
+    Tasks in replayscraper.py and replayfetcher.py
+    Add cli arg support to control what 3sDC.py does
 """
 import sys
 import json
@@ -16,9 +17,9 @@ import replayscraper
 challenge_id = None
 
 def main():
-    # fetcher = replayfetcher.Replayfetcher("../data/output.json")
-    # games = fetcher.get_username_replays("TheLetterH")
-    # fetcher.save_games_to_test_json(games)
+    fetcher = replayfetcher.Replayfetcher("../data/output.json")
+    games = fetcher.get_username_replays("TheLetterH")
+    fetcher.save_games_to_test_json(games)
 
     scraper = replayscraper.Replayscraper("../data/test.json")
     scraper.scrape_replay("1649265467782-9148")
