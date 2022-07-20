9a9158e77321da2f3c920c27c4c3ddb9a63a706b
h <harrisonmstuart@gmail.com>
2022-07-20 15:45:32 -0400

    Overhaul scraper.lua
    information pertaining to the latest scraped frame will be printed
    on-screen, and functionality for scraping a frame has been moved from
    per_frame() to get_frame_table()

--- src/scraper.lua
+++ src/scraper.lua
@@ -14,6 +14,12 @@
 			capture additional values each frame, as well as values which are relevant on a replay/round/game level
 		verification
 			it can't be assumed that registerafter is only called once per frame, so make sure that the frame_number value is unique before adding the data to the output
+			this is also relevant for updating replay_frame_count
+		compartmentalize parts of per_frame()
+			create a get_frame_table function which returns a frame_table
+			create a print_frame_table function which outputs the contents of a frame table to the gui
+			create a function for adding frame_table to output.txt
+			create a functino for adding frame_table to output.json
 ]]
 
 -- if there's issues, try adding dkjson.lua to /fbneo/lua
@@ -29,7 +35,7 @@ match_state = 0x020154A7 --byte
 -- add these to the base addresses to get the relevant value for each player
 life_offset = 0x9F --byte
 
--- set up working json (fightcade api json)
+-- set up working json (fightcade api information about the replay being scraped)
 json_tempfile = io.input("../data/.working.json"):read('*a')
 working_json = json.decode(tostring(json_tempfile))
 
@@ -37,33 +43,81 @@ working_json = json.decode(tostring(json_tempfile))
 output_txt = io.open("../data/log.txt", "w")
 output_json = io.open("../data/log.json", "w")
 
+-- replay frame count
+-- emulator and ingame frame counts don't start at 0, but this will
+replay_frame = 0
+
+-- startpoint for the emu framecount
+initial_emu_framecount = 0
+
+-- first startup flag; used to make sure certain operations will only execute the first time on_start() is called
+first_startup = true
+
+-- projected duration of the replay in frames
+duration_in_frames = working_json["duration"] * 60
+
 -- output table
 output_table = {}
 
+
 -- runs when emulation starts, or is reset
 function on_start() 
-	output_txt:write("\nemulation started!\n\n")
+	output_txt:write("emulation started!\n")
 
+	-- only execute these operations during the initial startup
+	if first_startup == true then
+		output_txt:write("Initial emu frame: " .. emu.framecount() .. "\n\n")
+		initial_emu_framecount = emu.framecount()
+		first_startup = false
+	end
 end
+
 
 -- runs when the lua script ends
 -- note that this doesn't run if the window is closed before the lua ends
 function on_exit()
+	-- encode the table of frame tables to a string
+	-- write that string as a json files
 	output_table_json_string = json.encode(output_table, {indent = true})
 	output_json:write(output_table_json_string)
 
-	output_txt:write("just a quick test that on_exit is being entered")
+	-- close the output files being worked on
 	output_txt:close()
 	output_json:close()
 end
 
--- runs every frame (currently at the end)
+
+-- runs every frame (currently registered to run at the end of the frame, before the inputs for the next frame are grabbed)
 function per_frame()
+	frame = get_frame_table()
 
-	-- capture information about the game
+	-- update gui
+	gui.text(30, 30, "PROJECTED REPLAY DURATION: " .. duration_in_frames)
+	gui.text(30, 40, "     CURRENT REPLAY FRAME: " .. tostring(frame["replay frame"]))
+	gui.text(30, 60, "                IN MATCH?: " .. frame["in match"])
+	gui.text(30, 70, "                  P1 LIFE: " .. tostring(frame["p1 life"]))
+	gui.text(30, 80, "                  P2 LIFE: " .. tostring(frame["p2 life"]))
+
+	-- write scraped frame information to output_txt
+	output_txt:write("ingame frame: " .. tostring(frame["ingame frame"]) .. "\n")
+	output_txt:write("emu frame: " .. tostring(frame["emu frame"]) .. "\n")
+	output_txt:write("p1 life: " .. tostring(frame["p1 life"]) .. "\n")
+	output_txt:write("p2 life: " .. tostring(frame["p2 life"]) .. "\n")
+	output_txt:write("in match?: " .. frame["in match"] .. "\n")
+	output_txt:write("\n")
+
+	-- add frame as a entry on output table
+	-- since the replay frame is persistent across resets, it will serve as the key 
+	output_table[frame["replay frame"]] = frame
+end
+
+
+-- returns a table with information about the current frame
+function get_frame_table()
+	frame_table = {}
+
+	-- scrape information about the frame
 	ingame_frame = memory.readdword(frame_number)
-	emu_frame = emu.framecount()
-
 	p1_life = memory.readbyte(p1_base + life_offset)
 	p2_life = memory.readbyte(p2_base + life_offset)
 
@@ -74,24 +128,17 @@ function per_frame()
 		in_match = "no"
 	end
 
-	-- write captured information to output_txt
-	output_txt:write("ingame frame: " .. tostring(ingame_frame) .. "\n")
-	output_txt:write("emu frame: " .. tostring(emu_frame) .. "\n")
-	output_txt:write("p1 life: " .. tostring(p1_life) .. "\n")
-	output_txt:write("p2 life: " .. tostring(p2_life) .. "\n")
-	output_txt:write("in match?: " .. tostring(in_match) .. "\n")
-	output_txt:write("\n")
+	-- calculate replay frame
+	replay_frame = emu.framecount() - initial_emu_framecount
 
-	-- write captured information to a frame table
-	frame = {}
-	frame["p1 life"] = p1_life
-	frame["p2 life"] = p2_life
-	frame["ingame frame"] = ingame_frame
-	frame["in match"] = in_match
-	
-	-- add the frame table to the output table
-	-- since the emu framecount is persistent across resets, it will serve as the key 
-	output_table[emu_frame] = frame
+	-- add values to frame_table
+	frame_table["p1 life"] = p1_life
+	frame_table["p2 life"] = p2_life
+	frame_table["ingame frame"] = ingame_frame
+	frame_table["in match"] = in_match
+	frame_table["replay frame"] = replay_frame
+
+	return frame_table
 end
 
 emu.registerstart(on_start)
